6cd232070277ae1c9e386c7e375288bfe3c1b717
ETEMESI <samweletemesi2@Gmail.com>
2023-12-14 05:11:56 +0300

    Completed Assignment.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+var ride = 199;
+function scuberGreetingForFeet (ride){
   // Write your code here!
+  
+  if (ride <= '400'){
+    return 'This one is on me!';
+  } else if (ride >= 2001){
+    return 'I will gladly take your thirty bucks.'
+  }else if (ride >= 2501){
+    return 'No can do.'
+  }
+
+  //console.log(ride);
+}
+ 
+function ternaryCheckCity(NYC){
+  // Write your code here!
+  return NYC <= 'NYC'?'Ok, sounds good.': 'No go.'
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(generous){
   // Write your code here!
+  switch(generous){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+console.log(scuberGreetingForFeet);
