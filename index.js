fea949e9624dd04bf14747a8c678f3c861c2f50a
ETEMESI <samweletemesi2@Gmail.com>
2024-01-07 09:24:23 +0300

    All tests passing after corrections.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet (ride){
   
   if (ride <= '400'){
     return 'This one is on me!';
-  } else if (ride >= 2001){
+  } else if (ride >= 2001 && ride <= 2500){
     return 'I will gladly take your thirty bucks.'
   }else if (ride >= 2501){
     return 'No can do.'
